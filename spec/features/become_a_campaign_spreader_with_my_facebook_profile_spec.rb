1958b117cd28063fda49d40655f10fe4c550aa95
Nícolas Iensen <nicolas@iensen.me>
2014-08-15 12:16:23 -0300

    [#62586254] refactoring tests

--- spec/features/become_a_campaign_spreader_with_my_facebook_profile_spec.rb
+++ spec/features/become_a_campaign_spreader_with_my_facebook_profile_spec.rb
@@ -6,6 +6,7 @@ feature "Become a campaign spreader with my Facebook profile", :type => :feature
   let(:facebook_uid){ "123" }
   let(:expires_at){ 1321747205 }
   let(:token){ "abcde" }
+  let(:ip){ "192.168.0.1" }
 
   before do
     OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
@@ -20,76 +21,138 @@ feature "Become a campaign spreader with my Facebook profile", :type => :feature
         expires_at: expires_at
       }
     })
-  end
-
-  scenario "when I'm a new user" do
-    ip = "192.168.0.1"
 
     allow_any_instance_of(ActionDispatch::Request).to receive(:remote_ip).and_return(ip)
+  end
 
-    visit campaign_path(campaign)
-    fill_in "campaign_spreader[timeline][user][email]", with: email
-    click_button "facebook-profile-campaign-spreader-submit-button"
+  context "when I'm a new user" do
+    around(:each) { @me = User.find_by_email(email) }
 
-    user = User.find_by_email(email)
+    scenario "it should create an user with my email" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
 
-    expect(user).to_not be_nil
-    expect(user.ip).to be_eql(ip)
-    expect(user.facebook_profile).to_not be_nil
-    expect(user.facebook_profile.uid).to be_eql(facebook_uid)
-    expect(user.facebook_profile.campaign_spreaders).to have(1).campaign_spreader
-    expect(campaign.campaign_spreaders).to have(1).campaign_spreader
-    expect(current_path).to be_eql(campaign_path(campaign))
-    expect(page).to have_css(".alert-box")
-    expect(page.get_rack_session['campaign_spreader']).to be_nil
+      expect(@me).to_not be_nil
+    end
+
+    scenario "it should save my ip address" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
+
+      expect(@me.ip).to be_eql(ip)
+    end
+
+    scenario "it should create a Facebook profile for me" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
+
+      expect(@me.facebook_profile).to_not be_nil
+    end
+
+    scenario "it should save my Facebook profile uid" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
+
+      expect(@me.facebook_profile.uid).to be_eql(facebook_uid)
+    end
+
+    scenario "it should make me a campaign spreader" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
+
+      expect(@me.facebook_profile.campaign_spreaders).to have(1).campaign_spreader
+    end
+
+    scenario "it should add a campaign spreader to the campaign" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
+
+      expect(campaign.campaign_spreaders).to have(1).campaign_spreader
+    end
+
+    scenario "it should redirect me to the campaign page" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
+
+      expect(current_path).to be_eql(campaign_path(campaign))
+    end
+
+    scenario "it should show me the alert box" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
+
+      expect(page).to have_css(".alert-box")
+    end
+
+    scenario "it should clean up my session" do
+      visit campaign_path(campaign)
+      fill_in "campaign_spreader[timeline][user][email]", with: email
+      click_button "facebook-profile-campaign-spreader-submit-button"
+
+      expect(page.get_rack_session['campaign_spreader']).to be_nil
+    end
   end
 
   context "when I'm not a new user" do
     before { @user = User.make! email: email }
 
     context "when I'm not logged in" do
-      scenario "when I don't have a Facebook profile" do
-        expect(User).to_not receive(:create)
+      context "when I don't have a Facebook profile" do
+        scenario "it should create Facebook profile for me" do
+          visit campaign_path(campaign)
+          fill_in "campaign_spreader[timeline][user][email]", with: email
+          click_button "facebook-profile-campaign-spreader-submit-button"
 
-        visit campaign_path(campaign)
-        fill_in "campaign_spreader[timeline][user][email]", with: email
-        click_button "facebook-profile-campaign-spreader-submit-button"
-
-        user = User.find_by_email(email)
-
-        expect(user.facebook_profile).to_not be_nil
-        expect(user.facebook_profile.uid).to be_eql(facebook_uid)
-        expect(user.facebook_profile.campaign_spreaders).to have(1).campaign_spreader
-        expect(campaign.campaign_spreaders).to have(1).campaign_spreader
-        expect(current_path).to be_eql(campaign_path(campaign))
-        expect(page).to have_css(".alert-box")
-        expect(page.get_rack_session['campaign_spreader']).to be_nil
+          expect(@user.facebook_profile).to_not be_nil
+        end
       end
 
-      scenario "when I have a Facebook profile" do
-        facebook_profile = FacebookProfile.make! user_id: @user.id, uid: facebook_uid
+      context "when I have a Facebook profile" do
+        before { @facebook_profile = FacebookProfile.make! user_id: @user.id, uid: facebook_uid }
 
-        visit campaign_path(campaign)
-        fill_in "campaign_spreader[timeline][user][email]", with: email
-        click_button "facebook-profile-campaign-spreader-submit-button"
+        scenario "it should update my Facebook profile expiring date" do
+          visit campaign_path(campaign)
+          fill_in "campaign_spreader[timeline][user][email]", with: email
+          click_button "facebook-profile-campaign-spreader-submit-button"
 
-        expect(facebook_profile.reload.expires_at).to be_eql(Time.at(expires_at))
-        expect(facebook_profile.reload.token).to be_eql(token)
+          expect(@facebook_profile.reload.expires_at).to be_eql(Time.at(expires_at))
+        end
+
+        scenario "it should update my Facebook profile token" do
+          visit campaign_path(campaign)
+          fill_in "campaign_spreader[timeline][user][email]", with: email
+          click_button "facebook-profile-campaign-spreader-submit-button"
+
+          expect(@facebook_profile.reload.token).to be_eql(token)
+        end
       end
     end
 
     context "when I'm logged in" do
       before { page.set_rack_session('cas' => {'user' => @user.email}) }
 
-      scenario "when I don't have a Facebook profile" do
-        visit campaign_path(campaign)
-        expect(page).to_not have_css("input[name='campaign_spreader[timeline][user][email]']")
+      context "when I don't have a Facebook profile" do
+        scenario "it should not show the user email field" do
+          visit campaign_path(campaign)
+          expect(page).to_not have_css("input[name='campaign_spreader[timeline][user][email]']")
+        end
 
-        click_button "facebook-profile-campaign-spreader-submit-button"
-        expect(@user.facebook_profile).to_not be_nil
+        scenario "it should create a Facebook profile for me" do
+          visit campaign_path(campaign)
+          click_button "facebook-profile-campaign-spreader-submit-button"
+          expect(@user.facebook_profile).to_not be_nil
+        end
       end
 
-      scenario "when I have a Facebook profile" do
+      context "when I have a Facebook profile" do
       end
     end
   end
