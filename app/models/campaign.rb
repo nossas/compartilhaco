5d64c59f30d1035a71d699736440c54cb2aaa817
Nícolas Iensen <nicolas@iensen.me>
2014-08-20 08:45:14 -0300

    [#75431346] add Campaign.unshared scope

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,8 @@ class Campaign < ActiveRecord::Base
   has_many :campaign_spreaders
   mount_uploader :image, ImageUploader
 
+  scope :unshared, -> { where("shared_at IS NULL") }
+
   def share
     campaign_spreaders.each {|cs| cs.share}
     update_attribute :shared_at, Time.now
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -3,6 +3,24 @@ require 'rails_helper'
 RSpec.describe Campaign, :type => :model do
   it { should have_many :campaign_spreaders }
 
+  describe ".unshared" do
+    context "when there is at least one unshared campaign" do
+      before { Campaign.make! }
+
+      it "should have one campaign" do
+        expect(Campaign.unshared).to have(1).campaign
+      end
+    end
+
+    context "when there is no unshared campaign" do
+      before { Campaign.make! shared_at: Time.now }
+
+      it "should be empty" do
+        expect(Campaign.unshared).to be_empty
+      end
+    end
+  end
+
   describe "#share" do
     subject { Campaign.make! }
     before { @campaign_spreader = CampaignSpreader.make!(:facebook_profile, campaign: subject) }
