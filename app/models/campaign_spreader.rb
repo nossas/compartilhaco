64cd7e8688b9e8c9c0c368cc2af5cddf6f72d05e
Nícolas Iensen <nicolas@iensen.me>
2014-08-20 12:02:46 -0300

    Add validations for CampaignSpreader

--- app/models/campaign_spreader.rb
+++ app/models/campaign_spreader.rb
@@ -2,6 +2,9 @@ class CampaignSpreader < ActiveRecord::Base
   belongs_to :timeline, polymorphic: true
   belongs_to :campaign
 
+  validates :timeline_id, :timeline_type, :campaign_id, presence: true
+  validates :timeline_id, uniqueness: { scope: [:timeline_type, :campaign_id] }
+
   def share
     timeline.share(self)
   end
--- spec/models/campaign_spreader_spec.rb
+++ spec/models/campaign_spreader_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 RSpec.describe CampaignSpreader, :type => :model do
   it { should belong_to :timeline }
   it { should belong_to :campaign }
+  it { should validate_presence_of :timeline_id }
+  it { should validate_presence_of :timeline_type }
+  it { should validate_presence_of :campaign_id }
+
+  context "when there is one campaign spreader" do
+    before { CampaignSpreader.make!(:facebook_profile) }
+    it { should validate_uniqueness_of(:timeline_id).scoped_to([:timeline_type, :campaign_id]) }
+  end
 
   describe "#share" do
     subject { CampaignSpreader.make!(:facebook_profile) }
