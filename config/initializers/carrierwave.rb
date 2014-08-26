c319b4daeac3c9559d6f63daecd5e1dcfaa6f081
Nícolas Iensen <nicolas@iensen.me>
2014-08-26 11:49:04 -0300

    [#62586228] add config to carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ CarrierWave.configure do |config|
   else
     config.storage    = :aws
     config.aws_bucket = ENV['AWS_BUCKET']
+    config.aws_acl    = :public_read
     config.aws_credentials = {
       access_key_id:     ENV['AWS_ID'],
       secret_access_key: ENV['AWS_SECRET']
