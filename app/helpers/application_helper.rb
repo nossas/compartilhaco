7e0af100b7d3556b9d3251a4fae86e26fa475791
Nícolas Iensen <nicolas@iensen.me>
2014-08-28 10:38:02 -0300

    [#62586228] fix helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def campaign_progress campaign
-    ((15.days.from_now - campaign.created_at)/(campaign.ends_at - campaign.created_at)) * 100
+    ((Time.now - campaign.created_at)/(campaign.ends_at - campaign.created_at)) * 100
   end
 end
