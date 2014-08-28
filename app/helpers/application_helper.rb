c1f29c1eeba595bc0a6adb3cd139915433d31acf
Nícolas Iensen <nicolas@iensen.me>
2014-08-28 12:49:49 -0300

    [#62586228] add user_path helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def campaign_progress campaign
     ((Time.now - campaign.created_at)/(campaign.ends_at - campaign.created_at)) * 100
   end
+
+  def user_path user
+    "#{ENV['MEURIO_HOST']}/users/#{user.id}"
+  end
 end
