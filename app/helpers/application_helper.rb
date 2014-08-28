7c6760864a47058cb2e32c5af026070ecfc86084
Nícolas Iensen <nicolas@iensen.me>
2014-08-28 10:34:17 -0300

    [#62586228] move helpers to application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def campaign_progress campaign
+    ((15.days.from_now - campaign.created_at)/(campaign.ends_at - campaign.created_at)) * 100
+  end
 end
--- app/helpers/campaign_spreaders_helper.rb
+++ app/helpers/campaign_spreaders_helper.rb
@@ -1,5 +1,2 @@
 module CampaignSpreadersHelper
-  def campaign_progress campaign
-    ((Time.now - campaign.created_at)/(campaign.ends_at - campaign.created_at)) * 100
-  end
 end
