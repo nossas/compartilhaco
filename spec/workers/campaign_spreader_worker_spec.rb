c92f4c77a59f2ecd545e1d2027303906e83965c3
Nícolas Iensen <nicolas@iensen.me>
2014-08-30 10:17:21 -0300

    Send new_spreader mail after CampaignSpreader is created

--- spec/workers/campaign_spreader_worker_spec.rb
+++ spec/workers/campaign_spreader_worker_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.describe CampaignSpreaderWorker, :type => :worker do
   describe "#perform" do
-    let(:campaign_spreader) { double("campaign_spreader", id: 1) }
+    let(:campaign_spreader) { CampaignSpreader.make!(:facebook_profile) }
     before { allow(CampaignSpreader).to receive(:find).with(campaign_spreader.id).and_return(campaign_spreader) }
+    before { allow(campaign_spreader).to receive(:create_segment_subscription) }
 
     it "should create segment subscription" do
       expect(campaign_spreader).to receive(:create_segment_subscription)
+      subject.perform(campaign_spreader.id)
+    end
+
+    it "should deliver the new spreader mail" do
+      new_spreader_mail = double("new_spreader_mail")
+      allow(NotificationMailer).to receive(:new_spreader).with(campaign_spreader).and_return(new_spreader_mail)
+      expect(new_spreader_mail).to receive(:deliver)
       subject.perform(campaign_spreader.id)
     end
   end
