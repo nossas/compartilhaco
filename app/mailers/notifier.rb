118c8b36e019f9509e481fcf99eded997ef4781e
Nícolas Iensen <nicolas@iensen.me>
2014-09-04 12:04:13 -0300

    [#75435018] add mail layout

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,11 @@
 class Notifier < ActionMailer::Base
   default from: "from@example.com"
+  layout "mail"
 
   def new_spreader campaign_spreader
     @user = campaign_spreader.timeline.user
     @campaign = campaign_spreader.campaign
+    @organization = @campaign.organization
     mail(to: @user.email, subject: @campaign.title, from: @campaign.user.email)
   end
 end
