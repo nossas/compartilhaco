378ab88c27f99d70322ff4db7e09257d581db6ec
Josias Schneider <josias.ds@gmail.com>
2014-09-05 18:31:08 -0300

    [#62586642] show button Archive Campaign only to admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905191443) do
+ActiveRecord::Schema.define(version: 20140905212349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,11 +100,12 @@ ActiveRecord::Schema.define(version: 20140905191443) do
   end
 
   create_table "users", force: true do |t|
-    t.string "email"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "ip"
-    t.string "avatar"
+    t.string  "email"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.string  "ip"
+    t.string  "avatar"
+    t.boolean "admin"
   end
 
 end
