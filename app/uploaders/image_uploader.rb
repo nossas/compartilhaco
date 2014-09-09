7f55afde3837b8461cbeaab1afee45d89bf4fe3b
Nícolas Iensen <nicolas@iensen.me>
2014-09-09 13:46:24 -0300

    [#62586214] change ImageUploader thumb size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -29,7 +29,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [500, 280]
+    process :resize_to_fill => [600, 350]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
