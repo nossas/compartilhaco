79756f9cf762a28682c9cc1d37931ddf58c5230a
Nícolas Iensen <nicolas@iensen.me>
2014-09-12 19:58:13 -0300

    [#75431036] add user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   it { should have_one :facebook_profile }
   it { should have_one :twitter_profile }
+  it { should have_many :spam_reports }
 end
