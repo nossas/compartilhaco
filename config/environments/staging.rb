3dbd5ac2d1c5626d2cb91bd7357f0a242e302fff
Nícolas Iensen <nicolas@iensen.me>
2014-09-12 11:43:12 -0300

    [#78482978] add default_url_options to staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.routes.default_url_options[:host] = "http://compartilhaco-staging.herokuapp.com/"
 end
