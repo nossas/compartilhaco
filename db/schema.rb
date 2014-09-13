9a3cb6241e654c4216b98d4d0a03e1f00673b40d
Nícolas Iensen <nicolas@iensen.me>
2014-09-13 19:33:05 -0300

    [#75431692] add hashtag to campaign

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912224446) do
+ActiveRecord::Schema.define(version: 20140913223223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140912224446) do
     t.text     "tweet",                      null: false
     t.text     "new_campaign_spreader_mail", null: false
     t.datetime "archived_at"
+    t.string   "hashtag"
     t.index ["category_id"], :name => "fk__campaigns_category_id"
     t.foreign_key ["category_id"], "categories", ["id"], :on_update => :no_action, :on_delete => :no_action, :name => "fk_campaigns_category_id"
   end
