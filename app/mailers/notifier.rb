604368462f773a96d6e6325e116d22e55eb9d33b
Nícolas Iensen <nicolas@iensen.me>
2014-09-16 14:37:02 -0300

    Refactoring Notifier

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,27 +6,41 @@ class Notifier < ActionMailer::Base
     @user = campaign_spreader.timeline.user
     @campaign = campaign_spreader.campaign
     @organization = @campaign.organization
-    mail(to: @user.email, subject: "Parabéns, você acaba de dar voz a um Compartilhaço!", from: @campaign.user.email)
+    mail(
+      to: @user.email,
+      subject: "Parabéns, você acaba de dar voz a um Compartilhaço!",
+      from: @campaign.user.email
+    )
   end
 
   def new_spam_report spam_report
     @user = spam_report.user
     @campaign = spam_report.campaign
-    mail(to: ENV["TECH_TEAM_EMAIL"], subject: "[Compartilhaço] Nova denúncia", from: @user.email)
+    mail(
+      to: ENV["TECH_TEAM_EMAIL"],
+      subject: "[Compartilhaço] Nova denúncia",
+      from: @user.email
+    )
   end
 
   def new_campaign campaign
     @campaign = campaign
     @user = campaign.user
     @organization = campaign.organization
-    mail(to: @user.email, subject: "Que legal, seu Compartilhaço foi criado com sucesso!")
+    mail(
+      to: @user.email,
+      subject: "Que legal, seu Compartilhaço foi criado com sucesso!"
+    )
   end
 
   def unsucceed_campaign_to_spreaders campaign
     @campaign = campaign
     @user = campaign.user
     @organization = campaign.organization
-    mail(bcc: campaign.campaign_spreaders.map{|cs| cs.user.email}, subject: "Que pena, o Compartilhaço que você apoiou não bateu sua meta de disparo!")
+    mail(
+      bcc: campaign.campaign_spreaders.map{|cs| cs.user.email},
+      subject: "Que pena, o Compartilhaço que você apoiou não bateu sua meta de disparo!"
+    )
   end
 
   def unsucceed_campaign_to_creator campaign
