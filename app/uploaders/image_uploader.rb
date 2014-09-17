791f7dee162522347474de392557518229fd0343
Nícolas Iensen <nicolas@iensen.me>
2014-09-17 12:21:37 -0300

    Add wider image type

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,6 +32,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [600, 350]
   end
 
+  version :wide do
+    process :resize_to_fill => [900, 400]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
