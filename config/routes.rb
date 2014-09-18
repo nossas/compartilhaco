d5372ccc9104c3f3f7241603979349bf6033385e
Josias Schneider <josias.ds@gmail.com>
2014-09-18 16:11:13 -0300

    [#75429848] add PagesController and about page

--- config/routes.rb
+++ config/routes.rb
@@ -13,15 +13,15 @@ Rails.application.routes.draw do
   resources :campaign_spreaders do
     collection do
       post(
-        'create_for_facebook_profile',
-        to: 'campaign_spreaders#create_for_facebook_profile',
-        as: :create_for_facebook_profile
+      'create_for_facebook_profile',
+      to: 'campaign_spreaders#create_for_facebook_profile',
+      as: :create_for_facebook_profile
       )
 
       post(
-        'create_for_twitter_profile',
-        to: 'campaign_spreaders#create_for_twitter_profile',
-        as: :create_for_twitter_profile
+      'create_for_twitter_profile',
+      to: 'campaign_spreaders#create_for_twitter_profile',
+      as: :create_for_twitter_profile
       )
     end
   end
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
   get '/auth/facebook/callback', to: 'campaign_spreaders#create_for_facebook_profile_callback'
   get '/auth/twitter/callback', to: 'campaign_spreaders#create_for_twitter_profile_callback'
   get '/auth/failure', to: 'campaign_spreaders#failure'
+
+  get '/about', to: "pages#about", as: :about
 
   get ENV['CAS_SERVER_URL'] => 'campaigns#index', as: :login if Rails.env.test?
 end
