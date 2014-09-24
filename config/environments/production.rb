2a22c187a271ed7c315b66255672292e7c331e04
Nícolas Iensen <nicolas@iensen.me>
2014-09-24 13:39:45 -0300

    Add default_url_options[:host]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.routes.default_url_options[:host] = "http://compartilhaco.minhascidades.org.br"
 end
