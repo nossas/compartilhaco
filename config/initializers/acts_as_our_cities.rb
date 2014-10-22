14b5ae4ad2c2e79ca5c21b3087aef3fcccfb72b6
Nícolas Iensen <nicolas@iensen.me>
2014-10-22 15:58:08 -0200

    [#80288298] deactivate acts_as_our_cities api mode when it's test or development env

--- config/initializers/acts_as_our_cities.rb
+++ config/initializers/acts_as_our_cities.rb
@@ -1,5 +1,5 @@
 ActsAsOurCities.config do |config|
-  config.api_mode = true
+  config.api_mode = Rails.env.production? || Rails.env.staging?
   config.api_site = ENV["ACCOUNTS_API_URL"]
   config.api_token = ENV["ACCOUNTS_API_TOKEN"]
 end
