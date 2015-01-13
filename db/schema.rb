931b957c510a8d6689af725019d76b8b32a4e43c
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-13 09:32:14 -0200

    [#86003772] run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102134912) do
+ActiveRecord::Schema.define(version: 20150113112637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "categories", force: true do |t|
     t.datetime "created_at"
