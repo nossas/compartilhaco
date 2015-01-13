21eb2bc98a1aee4c9e9d844b1b7fc6f2896037be
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-13 09:41:01 -0200

    [#86003772] create user mapping for cobalt database in meurio server

---
+++ db/migrate/20150113113916_create_user_mapping_for_cobalt_database_in_meurio_server.rb
@@ -0,0 +1,23 @@
+class CreateUserMappingForCobaltDatabaseInMeurioServer < ActiveRecord::Migration
+  def up
+    if Rails.env.production?
+      execute <<-SQL
+      CREATE USER MAPPING for #{ENV["DB_USERNAME"]}
+      SERVER meurio
+      OPTIONS (
+        user      '#{ENV["MEURIO_ACCOUNTS_DBUSER"]}',
+        password  '#{ENV["MEURIO_ACCOUNTS_DBPASS"]}'
+      );
+      SQL
+    end
+  end
+
+  def down
+    if Rails.env.production?
+      execute <<-SQL
+      DROP USER MAPPING IF EXISTS FOR #{ENV["DB_USERNAME"]}
+      SERVER meurio
+      SQL
+    end
+  end
+end
