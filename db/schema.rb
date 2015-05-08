077e0dc719d5b33b01cc97f32e3d3b7b2ba85257
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-05-08 12:29:21 -0300

    [#91974076] removed mailchimp_list_id from orgnaizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420181033) do
+ActiveRecord::Schema.define(version: 20150508152848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 20150420181033) do
   create_table "organizations", force: true do |t|
     t.text   "email_signature_html"
     t.string "city"
-    t.string "mailchimp_list_id"
     t.string "avatar"
     t.string "name"
   end
