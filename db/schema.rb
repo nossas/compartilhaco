7fcd8a4ec341d0366e44893905ae5d9f13d2a3b1
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-05-25 13:25:19 -0300

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150521201720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "categories", force: true do |t|
     t.datetime "created_at"
@@ -135,6 +136,8 @@ ActiveRecord::Schema.define(version: 20150521201720) do
     t.boolean "admin"
     t.string  "password"
     t.integer "organization_id"
+    t.index ["organization_id"], :name => "fk__users_organization_id"
+    t.foreign_key ["organization_id"], "organizations", ["id"], :on_update => :no_action, :on_delete => :no_action, :name => "fk_users_organization_id"
   end
 
 end
